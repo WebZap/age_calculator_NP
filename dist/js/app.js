104a3944a38355ed46071e50699789e1b5fb81e6
Maady <vlad030911@gmail.com>
2023-07-23 20:13:30 +0300

    add js date

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,9 @@
 "use strict";
+const {log} = console;
 
-alert("Hello Developer!");
+const addData = () => {
+    const date = new Date(2011,2,5);
+    return `год: ${date.getFullYear()}, Месяц ${date.getMonth() + 1}`
+} 
+
+log(addData())
