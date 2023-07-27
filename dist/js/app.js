869dc3d84fb2a973b8c83fb66227576a5128b4b7
Maady <vlad030911@gmail.com>
2023-07-27 12:16:11 +0300

    create validation

--- dist/js/app.js
+++ dist/js/app.js
@@ -6,8 +6,42 @@ const addData = () => {
     return `год: ${date.getFullYear()}, Месяц ${date.getMonth() + 1}`
 } 
 
-const test = () => {
-    alert('test')
-}
 
-log(addData())
+// log(addData())
+
+// add DOM elements
+
+const inputDay = document.getElementById('input-day');
+const inputMonth = document.getElementById('input-month');
+const inputYear = document.getElementById('input-year');
+
+// btn
+const btn = document.getElementById('button');
+
+// Validation
+
+btn.addEventListener('click', () =>{
+    const conditionDay = (value) =>{
+        const testOutDay = value > 31 
+        return testOutDay
+    }
+
+    const conditionMonth = (value) =>{
+        const testOutMonth = value >= 12 || value < 0 
+        return testOutMonth
+    }
+
+    const conditionYear = (value) => {
+        let myYear = (new Date()).getFullYear();
+        const testOutYear = value > myYear || value < 0;
+        return testOutYear
+    }
+
+    const test =  (value,condition) => {
+        const test = value === "" || condition(value) || isNaN(value) || value  === undefined;
+        return test
+    }
+    if(test(inputDay.value,conditionDay)){
+        alert(hello)
+    }
+})
