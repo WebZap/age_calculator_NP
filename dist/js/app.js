21a78718c92beac6882fc5a16344f036c001d191
Maady <vlad030911@gmail.com>
2023-07-27 14:17:16 +0300

    add validation

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,17 +10,55 @@ const addData = () => {
 // log(addData())
 
 // add DOM elements
+const inputContainers = document.querySelectorAll('.form__input-wrapper');
+const inputElements = document.querySelectorAll('.form__input');
 
 const inputDay = document.getElementById('input-day');
 const inputMonth = document.getElementById('input-month');
 const inputYear = document.getElementById('input-year');
 
+// labels
+
+const labels = document.querySelectorAll('.form__label');
+
 // btn
 const btn = document.getElementById('button');
 
 // Validation
+const clianing = () =>{
+inputElements.forEach((element, index) => {
+    element.addEventListener('input', () => {
+        element.classList.remove('err-input');
+        labels[index].classList.remove('err-label');
+        element.classList.remove('input-hover');
+        element.classList.remove('input-focus');
+    })
+})
+}
+
+const inputFocus = () =>{
+    inputElements.forEach((element, index) => {
+        element.addEventListener('focus', () => {
+            labels[index].classList.add('label-focus');
+        })
+        element.addEventListener('blur', () => {
+            labels[index].classList.remove('label-focus');
+        })
+    })
+}
+
+inputFocus()
+
+clianing()
+
 
 btn.addEventListener('click', () =>{
+    const classManipulation = (inputElement,inputNumber) => {
+        inputElement.classList.add('err-input');
+        inputElement.classList.add('input-focus');
+        inputElement.classList.add('input-hover');
+        labels[inputNumber - 1].classList.add('err-label');
+    }
     const conditionDay = (value) =>{
         const testOutDay = value > 31 
         return testOutDay
@@ -38,10 +76,32 @@ btn.addEventListener('click', () =>{
     }
 
     const test =  (value,condition) => {
-        const test = value === "" || condition(value) || isNaN(value) || value  === undefined;
+        const test =  condition(value) || isNaN(value) || value  === undefined || value === "" ;
         return test
     }
+
+    // if(test(inputDay.value,conditionDay) && test(inputMonth.value,conditionMonth) && test(inputMonth.value,conditionYear)){
+    //     classManipulation(inputDay, 1);
+    //     classManipulation(inputMonth, 2);
+    //     classManipulation(inputYear, 3);
+    // }
+
+    // if(test(inputDay.value,conditionDay) && test(inputMonth.value,conditionMonth)){
+    //     classManipulation(inputYear, 3);
+    // }
+
     if(test(inputDay.value,conditionDay)){
-        alert(hello)
+        classManipulation(inputDay, 1);
+        return;
     }
+    if(test(inputMonth.value,conditionMonth)){
+        classManipulation(inputMonth, 2);
+        return;
+    }
+    if(test(inputMonth.value,conditionYear)){
+        classManipulation(inputYear, 3);
+        return;
+    }
+
+
 })
